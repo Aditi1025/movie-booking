6d3d8f284b4580145a7d016ee50848c9c7a72365
Aditi Gupta <70718967+Aditi1025@users.noreply.github.com>
2023-01-12 11:17:07 +0530

    api movie add

---
+++ api/routes/movies.js
@@ -0,0 +1,77 @@
+import express from "express";
+
+// changed Movie stuff with movies and similarly changed the attributes for it as per ER-diagram
+import {
+  countByCity,
+  countByType,
+  createMovie,
+  deleteMovie,
+  getMovie,
+  getMovieRooms,
+  getMovies,
+  updateMovie,
+} from "../controllers/movie.js";
+
+import Movie from "../models/Movie.js";
+import { createError } from "../utils/error.js";
+// import {verifyAdmin} from "../utils/verifyToken.js"
+
+const router = express.Router();
+
+//CREATE
+// router.post("/", async (req,res) => {
+    
+//     const newMovie = new Movie(req.body)
+
+//     try{
+//         const savedMovie = await newMovie.save()
+//         res.status(200).json(savedMovie)
+//     }
+//     catch(err){
+//         res.status(500).json(err)
+//     }
+// })
+
+router.post("/", createMovie);
+
+// // //UPDATE
+// router.put("/:id", async (req,res) => {
+    
+//     try{
+//         const updatedMovie = await Movie.findByIdAndUpdate(req.params.id, 
+//             { $set: req.body},
+//             {new: true}
+//             );
+//         res.status(200).json(updatedMovie)
+//     }
+//     catch(err){
+//         res.status(500).json(err)
+//     }
+// })
+
+router.put("/:id", updateMovie);
+// // //DELETE
+router.delete("/:id",  deleteMovie);
+// // //GET
+
+router.get("/find/:id", getMovie);
+// // //GET ALL
+// router.get("/", async (req,res,next) => {
+//     const failed = true;
+
+//     if (failed)  return next(createError(401,"You are noth authenticated"));
+//     try{
+//         const Movies = await Movie.findById("ahsdkuhafse");
+//         res.status(200).json(Movies)
+//     }
+//     catch(err){
+//         next(err)
+//     }
+// })
+
+router.get("/", getMovies);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+router.get("/room/:id", getMovieRooms);
+
+export default router;
