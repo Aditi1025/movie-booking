0cfb94a3315fafd47bba86a71066ab9d268372ae
Bhaavya Rampal <invinciblebhaavya@gmail.com>
2023-01-25 01:02:31 +0530

    Login Auth

--- Clients/src/App.js
+++ Clients/src/App.js
@@ -6,6 +6,7 @@ import {
 import Home from "./pages/home/Home";
 import Movie from "./pages/movie/Movie";
 import List from "./pages/list/List";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/movies" element={<List/>}/>
-        <Route path="/movies/:id" element={<Movie/>}/>
+        <Route path="/movies/:id" element={<Movie />} />
+        <Route path="/login" element={<Login/>} />
       </Routes>
     </BrowserRouter>
   );
